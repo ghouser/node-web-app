8be71521e71244fffbf0ef716b64176a6486347e
ghouser <ghouser@iofficecorp.com>
2021-07-02 16:38:32 -0500

    continued work on demo

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sessions = require('./src/data/sessions.json');
 // pull express into a const
 const app = express();
+const PORT = process.env.PORT;
+const sessionsRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'/public/')));
 
-app.get('/',(req, res) => {
-    res.send('Hello from my app');
-})
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.listen(3000, () => {
-    debug(`listening on port ${chalk.green(3000)}`);
-})
+sessionsRouter.route('/')
+    .get((req,res) => {
+        res.render('sessions',{sessions});
+    })
+
+app.use('/sessions', sessionsRouter);
+
+app.get('/',(req, res) => {
+    res.render('index', { title: 'Globomantics', data: ['a','b','c'] });
+});
+
+app.listen(PORT, () => {
+    debug(`listening on port ${chalk.green(PORT)}`);
+});
